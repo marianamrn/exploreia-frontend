d11598d4e7f38637d32ca2469cda805b59fc8677
Mariana Marunchak <mariana.mrnch@gmail.com>
2024-12-14 13:59:27 +0200

    Set connection to other deskstop

--- vite.config.js
+++ vite.config.js
@@ -12,5 +12,11 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  server: {
+    host: '0.0.0.0', // Дозволяє доступ з інших пристроїв
+    port: 5173,      //  Порт 
+    strictPort: true, // Якщо порт зайнятий, не буде використовувати інший
+    open: true       // Автоматично відкриває браузер на моєму комп'ютері
   }
 })
